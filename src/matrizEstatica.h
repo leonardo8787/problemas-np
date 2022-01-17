25d2f8c652ee536919990cc3b670e100950a6e12
danieldiv <das.29@outlook.com>
2022-01-16 22:01:09 -0300

    removendo valores comentados

--- src/matrizEstatica.h
+++ src/matrizEstatica.h
@@ -13,20 +13,3 @@ int **inicializaMatriz(int **matriz, int op);
 void atribuiMatriz(int **matriz, int op, Lista *l);
 
 #endif
-// #ifndef ESTATICA_H
-// #define ESTATICA_H
-
-// #include <stdio.h>
-// #include <stdlib.h>
-
-// #define INFINITO 429496729
-
-// void teste();
-// // void imprimir();
-// // void caixeiroViajanteAux(int x);
-// // void caixeiroViajante(int inicial);
-// // void iniciaVetores(int tamanho);
-// // void preencheMatriz(Lista *l);
-// // void mallocar();
-
-// #endif
